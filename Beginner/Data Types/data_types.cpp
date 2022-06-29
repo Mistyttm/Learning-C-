ad2d3635eae8ff4b7b3283eff7eef13be052a240
MistyTTM <emmey.leo@gmail.com>
2022-06-29 16:13:14 +1000

    I dislike how slow this is going

---
+++ Beginner/Data Types/data_types.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    /*char always use ''*/
+    char grade = 'A';
+
+    /*String uses ""*/
+    string phrase = "This is a goddamn string";
+
+    /*int cannot use decimals*/
+    int age = 50;
+
+    /*uses decimal (more specific)*/
+    float badGpa = 1.1;
+
+    /*Double uses more decimal places than float*/
+    double gpa = 4.5;
+
+    /*It's just a boolean*/
+    bool isFemale = true;
+
+
+    /*god this course is slow*/
+
+    return 0;
+}
---
+++ Beginner/working_with_strings/working_with strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    /*endl tells C++ to add a newline (can also use \n)*/
+
+    string phrase = "goodbye World";
+    cout << phrase;
+
+    return 0;
+}
