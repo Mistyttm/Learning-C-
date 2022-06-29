f35bbcf528f90eadc306a11d35b44fce2d7d4dc7
MistyTTM <emmey.leo@gmail.com>
2022-06-29 15:56:50 +1000

    Variables Episode II

--- Beginner/Variables/Variables.cpp
+++ Beginner/Variables/Variables.cpp
@@ -5,14 +5,16 @@ using namespace std;
 int main(){
 
     /* Variables */
-    string animal = "Cat";
+    string animal = "Dog";
 
     int animalAge;
-    animalAge = 26;
+    animalAge = 14;
 
     /* Program */
     cout << "This is a story of a " << animal << endl;
     cout << "The " << animal << " was " << animalAge << endl;
+
+    animal = "Cat";
     cout << "The " << animal << " was a " << animal << endl;
     cout << "But he wasn't " << animalAge << endl;
 
