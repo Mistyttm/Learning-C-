421fa62f4b5a7a9b38f7ed9cf68a104dcb4157e9
MistyTTM <emmey.leo@gmail.com>
2022-06-30 23:01:38 +1000

    simple programs

---
+++ Beginner/mad_libs_game/madlibs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    string colour, pluralNoun, celebrity;
+
+    cout<< "Enter a colour: ";
+    getline(cin, colour);
+    cout<< "Enter a plural noun: ";
+    getline(cin, pluralNoun);
+    cout<< "Enter a celebrity: ";
+    getline(cin, celebrity);
+
+    cout << "Roses are " << colour << endl;
+    cout << pluralNoun << " are Blue" << endl;
+    cout << "I love " << celebrity << endl;
+
+    return 0;
+}
