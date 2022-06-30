17d813d5ad1aa450bbe8025739d4e03c0732ea74
MistyTTM <emmey.leo@gmail.com>
2022-06-30 18:56:52 +1000

    stinrgs are overrr

--- Beginner/working_with_strings/working_with strings.cpp
+++ Beginner/working_with_strings/working_with strings.cpp
@@ -7,7 +7,23 @@ int main(){
     /*endl tells C++ to add a newline (can also use \n)*/
 
     string phrase = "goodbye World";
-    cout << phrase;
+    cout << phrase << endl;
+
+    /*Prints length of the string*/
+    cout << phrase.length() << endl;
+
+    /*select individual characters in a string*/
+    cout << phrase[2] << endl;
+
+    phrase[0] = 'B';
+    cout << phrase << endl;
+
+    /*Returns which index the string occurs*/
+    cout << phrase.find("World", 0) << endl;
+
+    /*Selects number of characters from an index*/
+    cout << phrase.substr(8, 3) << endl;
+
 
     return 0;
 }
