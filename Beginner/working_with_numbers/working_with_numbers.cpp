35b22ee18217a26d21a89da3408ec75c50a4a775
MistyTTM <emmey.leo@gmail.com>
2022-06-30 22:23:07 +1000

    this one felt slightly unnecessary

---
+++ Beginner/working_with_numbers/working_with_numbers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    cout << 40 % 3 << endl;
+
+    /*Do I really need to follow this part of the tutorial?????*/
+    /*yes*/
+
+    int num = 5;
+    num++;
+    /*Okay nvm*/
+    /*scratch that again im gonna learn something useful*/
+
+    /*raise to the power*/
+    cout << pow(2,5) << endl;
+
+    /*squareroot*/
+    cout << sqrt(30) << endl;
+
+    /*Rounding*/
+    cout << round(4.6) << endl;
+
+    /*round up*/
+    cout << ceil(4.3) << endl;
+
+    /*round down*/
+    cout << floor(4.5) << endl;
+
+    /*pick the larger of the two*/
+    cout << fmax(2,5) << endl;
+
+    /*pick the larger of the two*/
+    cout << fmin(2,5) << endl;
+
+    return 0;
+}
